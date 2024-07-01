5c3b19a28b2af4426747b470008fc8ecc63d5448
krishna-vogeti <109520875+krishna-vogeti@users.noreply.github.com>
2024-07-01 07:40:16 +0530

    java testing learnings core and advanced

---
+++ Java_Learning/src/test/java/corejava_Basics/HelloJavaVariables.java
@@ -0,0 +1,14 @@
+package corejava_Basics;
+
+public class HelloJavaVariables {
+
+	public static void main(String[] args)
+	{
+		System.out.println("hello world");
+		
+		String firstName= "saikrishna";
+		String LastName= "vogeti";
+
+		System.out.println("print concatinated foll name:"+firstName+LastName);
+	}
+}
